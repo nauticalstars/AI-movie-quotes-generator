f5c6cd8c84889425f3a68039b3d6484c39fa8e9b
Nautical <nauticalstars21@gmail.com>
2024-03-31 13:50:04 -0700

    improved the AI context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateQuote(event) {
   let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
   let prompt = `Generate famous movies quotes from ${userInput.value} `;
   let context =
-    "You are a movie nerd that knows all the famous quotes from all movies and actors and actresses ever made. Please provide only 4 examples and the movies they are from, short one sentance quotes in basic HTML and separate each line with a <br />. Do not include search title.";
+    "You are a movie nerd that knows all the famous quotes from all movies and actors and actresses ever made. Please provide only 4 examples and the movies they are from, short one sentance quotes in basic HTML and separate each line with a <br />. Do not include search title. Sign your signature 'SheCodes AI' inside a <strong> element at the end of the search data.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayQuote);
 }
