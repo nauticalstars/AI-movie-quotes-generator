9f23ecd16b707b1ed80e6ac540ee80c81989e334
Nautical <nauticalstars21@gmail.com>
2024-03-31 13:03:38 -0700

    basic form handling and added Typewritter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "testing",
+    autoStart: true,
+    delay: 1,
+    cursor: " ",
+  });
+}
 
+let quoteForm = document.querySelector("#quote-generator-form");
+quoteForm.addEventListener("submit", generateQuote);
